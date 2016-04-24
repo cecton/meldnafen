250a0cfb4a35405d2126aca5c48d2f84d90732b5
Cecile Tonglet <cecile.tonglet@gmail.com>
2016-04-24 15:18:02 +0200

    Implements Joystick configuration (#3)
    Basic Component to configure a joytick

--- meldnafen/app.py
+++ meldnafen/app.py
@@ -8,8 +8,9 @@ import sdl2ui.mixins
 from sdl2ui.debugger import Debugger
 from sdl2ui.joystick import JoystickManager, KeyboardJoystick
 
-from meldnafen.menu import Menu
+from meldnafen.config.controls import Controls
 from meldnafen.list_roms import ListRoms
+from meldnafen.menu import Menu
 from meldnafen.vgm import VgmPlayer, VgmFile
 
 
@@ -71,13 +72,31 @@ class Meldnafen(sdl2ui.App, sdl2ui.mixins.ImmutableMixin):
         sdl2.SDL_SetHint(sdl2.SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, b"1")
         self.command = None
         self.joystick_manager = self.add_component(JoystickManager)
-        self.add_component(KeyboardJoystick,
+        self.joystick = self.add_component(KeyboardJoystick,
             manager=self.joystick_manager,
             index=0,
             keyboard_mapping={
                 k: getattr(sdl2, v)
                 for k, v in self.props['joystick']
-            }).enable()
+            })
+        self.joystick.enable()
+        self.joystick_configure = self.add_component(
+            Controls,
+            border=10,
+            line_space=10,
+            countdown=8,
+            on_finish=self.finish_joystick_configuration,
+            controls=[
+                ('up', "Up"),
+                ('down', "Down"),
+                ('left', "Left"),
+                ('right', "Right"),
+                ('ok', "OK"),
+                ('cancel', "Cancel"),
+                ('menu', "Menu"),
+                ('next_page', "Next page"),
+                ('prev_page', "Previous page"),
+            ])
         self._load_emulator_components()
         self.menu = self.add_component(Menu,
             actions=self.props['menu_actions'],
@@ -91,6 +110,7 @@ class Meldnafen(sdl2ui.App, sdl2ui.mixins.ImmutableMixin):
             sdl2.SDL_SCANCODE_Q: self.app.quit,
             sdl2.SDL_SCANCODE_D: self.toggle_debug_mode,
             sdl2.SDL_SCANCODE_ESCAPE: self.toggle_menu,
+            sdl2.SDL_SCANCODE_J: self.app.activate_joystick_configuration,
         }
         self.bgm = self._load_bgm()
         self.register_event_handler(sdl2.SDL_KEYDOWN, self.keypress)
@@ -112,6 +132,8 @@ class Meldnafen(sdl2ui.App, sdl2ui.mixins.ImmutableMixin):
         self.debugger.toggle()
 
     def toggle_menu(self):
+        if self.joystick_configure.active:
+            return
         self.emulators[self.state['emulator']].toggle()
         self.menu.toggle()
         self.bgm.toggle()
@@ -126,3 +148,29 @@ class Meldnafen(sdl2ui.App, sdl2ui.mixins.ImmutableMixin):
         self.emulators[self.state['emulator']].disable()
         self.emulators[index].enable()
         self.set_state({'emulator': index})
+
+    def lock(self):
+        self.menu_state = self.menu.active
+        self.menu.disable()
+        self.emulators[self.state['emulator']].disable()
+        self.joystick.disable()
+
+    def unlock(self):
+        if self.menu_state:
+            self.menu.enable()
+        else:
+            self.emulators[self.state['emulator']].enable()
+        self.joystick.enable()
+
+    def activate_joystick_configuration(self):
+        self.lock()
+        self.joystick_configure.enable()
+
+    def update_joystick_configuration(self, config):
+        self.logger.error("update: %r", config)
+
+    def finish_joystick_configuration(self, config=None):
+        if config:
+            self.update_joystick_configuration(config)
+        self.joystick_configure.disable()
+        self.unlock()
---
+++ meldnafen/config/controls.py
@@ -0,0 +1,131 @@
+import logging
+import sdl2
+import sdl2ui
+import sdl2ui.joystick
+import sdl2ui.mixins
+
+
+class JoystickCapture(sdl2ui.Component, sdl2ui.mixins.ImmutableMixin):
+    @property
+    def x(self):
+        return int((self.app.viewport.w - 256) / 2 + self.props['border'])
+
+    @property
+    def y(self):
+        return (
+            int((self.app.viewport.h - 224) / 2 + self.props['border']) +
+            self.props['line_space'] * 4
+        )
+
+    def init(self):
+        self.register_event_handler(
+            sdl2.SDL_JOYBUTTONDOWN, self.capture_button)
+
+    def capture_button(self, event):
+        if not event.jbutton.which == self.state['joystick'].id:
+            return
+        new_controls = self.state['controls'].copy()
+        control = new_controls.pop(0)
+        new_config = self.state['config'].copy()
+        new_config.update({
+            control[0]: event.jbutton.button,
+        })
+        self.set_state({
+            'config': new_config,
+            'x': control,
+        })
+        if not new_controls:
+            self.parent.stop_capture()
+        else:
+            self.set_state({
+                'controls': new_controls,
+            })
+
+    def render(self):
+        self.app.write('font-12', self.x, self.y, self.state['controls'][0][1])
+
+
+class Controls(sdl2ui.Component, sdl2ui.mixins.ImmutableMixin):
+    @property
+    def x(self):
+        return int((self.app.viewport.w - 256) / 2 + self.props['border'])
+
+    @property
+    def y(self):
+        return int((self.app.viewport.h - 224) / 2 + self.props['border'])
+
+    def init(self):
+        self.capture = self.add_component(JoystickCapture,
+            line_space=self.props['line_space'],
+            border=self.props['border'])
+        self.register_event_handler(sdl2.SDL_JOYDEVICEADDED, self.detect)
+        self.register_event_handler(sdl2.SDL_JOYBUTTONDOWN, self.button_down)
+
+    def update_countdown(self):
+        remaining = self.state['countdown'] - 1
+        self.set_state({
+            'countdown': remaining,
+        })
+        if remaining > 0:
+            self.app.add_timer(1000, self.update_countdown)
+        else:
+            self.cancel()
+
+    def detect(self, event):
+        index = event.jdevice.which
+        if index in self.joysticks:
+            return
+        self.app.joystick_manager.get(index).open()
+
+    def activate(self):
+        sdl2.SDL_JoystickUpdate()
+        self.set_state({
+            'countdown': self.props['countdown'],
+        })
+        self.app.add_timer(1000, self.update_countdown)
+
+    def button_down(self, event):
+        self.set_state({
+            'countdown': self.props['countdown'],
+        })
+        if not self.capture.active:
+            joysticks = filter(
+                lambda x: x.id == event.jdevice.which,
+                self.app.joystick_manager.joysticks.values())
+            joystick = next(iter(joysticks))
+            self.start_capture(joystick)
+
+    def start_capture(self, joystick):
+        self.set_state({
+            'name': joystick.name,
+        })
+        self.capture.set_state({
+            'joystick': joystick,
+            'controls': self.props['controls'].copy(),
+            'config': {},
+        })
+        self.capture.enable()
+
+    def stop_capture(self):
+        self.capture.disable()
+        self.props['on_finish'](self.capture.state['config'])
+
+    def cancel(self):
+        self.capture.disable()
+        self.props['on_finish']()
+
+    def render(self):
+        y = self.y
+        if self.capture.active:
+            self.app.write('font-12', self.x, y, self.state['name'])
+            y += 2 * self.props['line_space']
+            self.app.write('font-12', self.x, y, "Press the button...")
+        else:
+            self.app.write('font-12', self.x, y,
+                "Press any button to start configuring")
+        if self.state['countdown'] <= 5:
+            with self.app.tint((0xff, 0x00, 0x00, 0xff)):
+                y += 4 * self.props['line_space']
+                self.app.write('font-12', self.x, y,
+                    "Cancel configuration in %d seconds..."
+                    % self.state['countdown'])
